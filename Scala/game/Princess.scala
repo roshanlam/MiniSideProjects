95deb5f8b16868a4fa93ab0dfb7091d7a998fc78
Roshan Lamichhane <33913826+roshanlam@users.noreply.github.com>
2019-11-24 12:37:31 -0500

    Create Princess.scala

---
+++ Scala/game/Princess.scala
@@ -0,0 +1,10 @@
+class Princess (val name:String) {
+    def kiss(p:Player) = {
+        println(name + " gave a kiss to " + p.name)
+    }
+
+    def slap(p:Player) = {
+        println(name + " slapped " + p.name)
+        p.looseLife()
+    }
+}
