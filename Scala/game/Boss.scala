3b45edefa250ec7c0b616a0a562dd1ec84800dfe
Roshan Lamichhane <33913826+roshanlam@users.noreply.github.com>
2019-11-24 12:39:24 -0500

    Create Boss.scala

---
+++ Scala/game/Boss.scala
@@ -0,0 +1,10 @@
+class Boss (val name:String) {
+    def beat(e:Enemy) = {
+        println(name + " beat " + e.name)
+    }
+
+    def beat(p:Player) = {
+        println(name + " beat " + p.name)
+        p.looseLife()
+    }
+}
