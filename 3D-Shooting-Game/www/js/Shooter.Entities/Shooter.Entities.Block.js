17ccb0fd0db47c68d0ea270be0221936932c9f5d
Roshan Lamichhane <33913826+roshanlam@users.noreply.github.com>
2019-12-26 12:54:20 -0500

    Create Shooter.Entities.Block.js

---
+++ 3D-Shooting-Game/www/js/Shooter.Entities/Shooter.Entities.Block.js
@@ -0,0 +1,10 @@
+'use strict';
+Shooter.namespace("Shooter.Entities");
+Shooter.Entities.Block = class {
+	static create(height, width, depth) {
+		let geometry = new THREE.BoxGeometry(height, width, depth);
+		let instance = new THREE.Mesh(geometry);
+		return instance;
+	}
+};
+export default Shooter.Entities.Block;
