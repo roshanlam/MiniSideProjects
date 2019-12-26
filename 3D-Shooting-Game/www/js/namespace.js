5839cedc759c60916c271a80838f8339bcffbfe9
Roshan Lamichhane <33913826+roshanlam@users.noreply.github.com>
2019-12-26 12:44:46 -0500

    Create namespace.js

---
+++ 3D-Shooting-Game/www/js/namespace.js
@@ -0,0 +1,18 @@
+window.Shooter = ("undefined" === typeof Shooter) ? {} : Shooter;
+window.Shooter.namespace = function (namespace) {
+    let parts = namespace.split('.'),
+        parent = Shooter;
+
+    if ("Shooter" === parts[0]) {
+        parts = parts.slice(1);
+    }
+
+    for(let singlePart of parts) {
+        if ("undefined" === typeof parent[singlePart]) {
+            parent[singlePart] = {};
+        }
+        parent = parent[singlePart];
+    }
+
+    return parent;
+};
