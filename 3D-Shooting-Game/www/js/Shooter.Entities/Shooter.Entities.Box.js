99158c8a9e8a03ee0896baaf00e47725e6cb3e71
Roshan Lamichhane <33913826+roshanlam@users.noreply.github.com>
2019-12-26 12:54:55 -0500

    Create Shooter.Entities.Box.js

---
+++ 3D-Shooting-Game/www/js/Shooter.Entities/Shooter.Entities.Box.js
@@ -0,0 +1,16 @@
+'use strict';
+Shooter.namespace("Shooter.Entities");
+import Loader from '../Shooter.Graphics/Shooter.Graphics.Loader.js';
+Shooter.Entities.Box = class {
+	static create() {
+		let loader, geometry, material, mesh;
+		geometry = new THREE.BoxGeometry(3, 3, 3);
+		let box_texture = new THREE.Texture();
+		box_texture.image = Loader.getImage('box2');
+		box_texture.needsUpdate = true;
+		material = new THREE.MeshBasicMaterial({ map: box_texture, overdraw: true });
+		mesh = new THREE.Mesh(geometry, material);
+		return mesh;
+	}
+};
+export default Shooter.Entities.Box;
