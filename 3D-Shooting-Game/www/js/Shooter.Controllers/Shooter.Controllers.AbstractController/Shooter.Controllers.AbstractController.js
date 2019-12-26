0d8a4b4642bbc9bdbf872c2009158c7a8da8d3d7
Roshan Lamichhane <33913826+roshanlam@users.noreply.github.com>
2019-12-26 13:04:39 -0500

    Create Shooter.Controllers.AbstractController.js

---
+++ 3D-Shooting-Game/www/js/Shooter.Controllers/Shooter.Controllers.AbstractController/Shooter.Controllers.AbstractController.js
@@ -0,0 +1,12 @@
+'use strict';
+Shooter.namespace("Shooter.Controllers");
+Shooter.Controllers.AbstractController = class {
+	constructor() {
+		this.attachEvents();
+		console.log("> Shooter.Controllers.AbstractController > constructor > ready");
+	}
+	attachEvents() { }
+	detachEvents() { }
+};
+
+export default Shooter.Controllers.AbstractController;
