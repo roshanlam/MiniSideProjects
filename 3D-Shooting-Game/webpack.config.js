b7f9024b435d89cbdb4e7795b01f3561dd72b57e
Roshan Lamichhane <33913826+roshanlam@users.noreply.github.com>
2019-12-26 12:41:00 -0500

    Create webpack.config.js

---
+++ 3D-Shooting-Game/webpack.config.js
@@ -0,0 +1,34 @@
+'use strict';
+const NODE_ENV = process.env.NODE_ENV || 'development';
+const webpack = require('webpack');
+module.exports = {
+    context: __dirname + "/www/js",
+	entry: "./Shooter.Game/Shooter.Game",
+    output: {
+        path: __dirname + "/www/build",        
+        filename: "build.js"
+    },
+
+    devtool: NODE_ENV == 'development' ? 'inline-source-map' : null,
+    plugins: [ ],
+    module: {
+    	loaders: [{
+    		test: /\.js$/,
+    		loader: 'babel',
+    		query: {
+    			presets: ['es2015']
+    		}
+    	}]
+    }
+};
+
+if("production" === NODE_ENV) {
+    module.exports.plugins.push(
+        new webpack.optimize.UglifyJsPlugin({
+            compress: {
+                drop_console: true,
+                unsafe: true
+            }
+        })
+    );
+}
