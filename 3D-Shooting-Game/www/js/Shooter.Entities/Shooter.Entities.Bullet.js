34a2cf24c6b8ec4aa3de5aac7669bf0fea95b911
Roshan Lamichhane <33913826+roshanlam@users.noreply.github.com>
2019-12-26 12:55:31 -0500

    Create Shooter.Entities.Bullet.js

---
+++ 3D-Shooting-Game/www/js/Shooter.Entities/Shooter.Entities.Bullet.js
@@ -0,0 +1,26 @@
+'use strict';
+Shooter.namespace("Shooter.Entities");
+import CONSTANTS from '../Shooter.Constants/Shooter.Constants.js';
+Shooter.Entities.Bullet = class {
+	constructor(position, rotation, direction) {
+		this.geometry = new THREE.CylinderGeometry(CONSTANTS.BULLET.RADIUS, CONSTANTS.BULLET.RADIUS, CONSTANTS.BULLET.HEIGHT, CONSTANTS.BULLET.RADIUS_SEGMENTS);
+		this.material = new THREE.MeshBasicMaterial({ color: 'green' });
+		this.mesh = new THREE.Mesh(this.geometry, this.material);
+		this.mesh.rotation.set(Math.PI / 2, 0, 0);
+		this.instance = new THREE.Object3D();		
+		this.instance.add(this.mesh);
+		this.instance.rotation.set(rotation.x, rotation.y, rotation.z);
+		this.instance.position.set(position.x, position.y, position.z)
+		this.direction = direction;
+		this.direction.multiplyScalar(CONSTANTS.BULLET.SPEED);
+	}
+
+	update() {
+		this.instance.position.add(this.direction);
+	}
+
+	getInstance() {
+		return this.instance;
+	}
+};
+export default Shooter.Entities.Bullet;
