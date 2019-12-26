44b8692d471761badbd18a94636473bce53c8854
Roshan Lamichhane <33913826+roshanlam@users.noreply.github.com>
2019-12-26 12:48:21 -0500

    Create Shooter.Graphics.Renderer.js

---
+++ 3D-Shooting-Game/www/js/Shooter.Graphics/Shooter.Graphics.Renderer.js
@@ -0,0 +1,16 @@
+'use strict';
+Shooter.namespace("Shooter.Graphics");
+Shooter.Graphics.Renderer = class {
+	constructor() {
+		this.renderer = new THREE.WebGLRenderer();
+		this.renderer.setSize(window.innerWidth, window.innerHeight);
+		document.body.appendChild(this.renderer.domElement);
+		console.log("> Shooter.Graphics.Render > constructor > ready");
+	}
+
+	render(scene, camera) {
+		this.renderer.render( scene, camera );
+	}
+}
+
+export default Shooter.Graphics.Renderer;
